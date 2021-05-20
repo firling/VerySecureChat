235b59062d3c30e5ca600aa468dbae257179b044
Julien Anquetil <anquetil.jul@gmail.com>
2021-05-21 00:51:35 +0200

    removed db uri

--- server/db.js
+++ server/db.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 exports.connect = async () => {
-  //const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-  const uri = `mongodb+srv://vsc:W2gq0xNSX1zALxS6@cluster0.k4stz.mongodb.net/VerySecureChat?retryWrites=true&w=majority`;
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
   const conn = await mongoose.connect(uri, {
     useNewUrlParser: true,
     useFindAndModify: true,

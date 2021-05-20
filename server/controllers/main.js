9798561339bcf8ca84a07b414dd1f01b6986ff86
Hugo Lebreton <lebretonhugo@gmail.com>
2021-05-20 20:08:55 +0200

    WIP: cipher message in sendMessage

--- server/controllers/main.js
+++ server/controllers/main.js
@@ -3,85 +3,96 @@ const messageModel = require("../models/message.js");
 const socket = require("../socket");
 
 const getUsers = async (req, res, next) => {
-    const users = await userModel.find({
-        _id: {$ne: req.user._id}
-    }).select('_id name');;
-    return res.status(200).json({
-        users
-    });
-}
+  const users = await userModel
+    .find({
+      _id: { $ne: req.user._id },
+    })
+    .select("_id name");
+  return res.status(200).json({
+    users,
+  });
+};
 
 const compare = (a, b) => {
-    if (a.createdAt < b.createdAt)
-        return -1;
-    if (a.createdAt > b.createdAt)
-        return 1;
-    
-    return 0;
-}
+  if (a.createdAt < b.createdAt) return -1;
+  if (a.createdAt > b.createdAt) return 1;
+
+  return 0;
+};
 
 const getMessages = async (req, res, next) => {
-    const { corresponding_id } = req.params;
-    const messageSender = await messageModel.find({
-        sender: req.user._id,
-        receiver: corresponding_id,
-    })
-    newMessageSender = messageSender.map(elt => {
-        elt.author = "author";
-        return {
-            _id: elt._id,
-            author: "author",
-            sender: elt.sender,
-            receiver: elt.receiver,
-            message: elt.message,
-            createdAt: elt.createdAt,
-        };
-    }) 
-    const messageReceiver = await messageModel.find({
-        sender: corresponding_id,
-        receiver: req.user._id,
-    })
+  const { corresponding_id } = req.params;
+  const messageSender = await messageModel.find({
+    sender: req.user._id,
+    receiver: corresponding_id,
+  });
+  newMessageSender = messageSender.map((elt) => {
+    elt.author = "author";
+    return {
+      _id: elt._id,
+      author: "author",
+      sender: elt.sender,
+      receiver: elt.receiver,
+      message: elt.message,
+      createdAt: elt.createdAt,
+    };
+  });
+  const messageReceiver = await messageModel.find({
+    sender: corresponding_id,
+    receiver: req.user._id,
+  });
 
-    const message = [...newMessageSender, ...messageReceiver]
+  const message = [...newMessageSender, ...messageReceiver];
 
-    message.sort(compare);
+  message.sort(compare);
 
-    return res.status(200).json({
-        message
-    })
-}
+  return res.status(200).json({
+    message,
+  });
+};
 
 const sendMessage = async (req, res, next) => {
-    const { corresponding_id, message } = req.body;
+  const { corresponding_id, message } = req.body;
 
-    messageModel.create({
-        sender: req.user._id,
-        receiver: corresponding_id,
-        message: message
-    })
+  const sender = await userModel.findOne({ _id: req.user._id });
+  const receiver = await userModel.findOne({ _id: corresponding_id });
+  const { publicKey: senderPublicKey, privateKey: senderPrivateKey } =
+    sender.settings;
+  const { publicKey: receiverPublicKey, privateKey: receiverPrivateKey } =
+    receiver.settings;
 
-    if (socket.getCorresponding()[corresponding_id]) {
-        console.log(socket.getCorresponding()[corresponding_id]);
-        socket.getIo().to(socket.getCorresponding()[corresponding_id]).emit("newMessage");
-    }
+  messageModel.create({
+    sender: req.user._id,
+    receiver: corresponding_id,
+    senderMessage: message,
+    receiverMessage: message,
+  });
 
-    return res.status(200).end()
-}
+  if (socket.getCorresponding()[corresponding_id]) {
+    console.log(socket.getCorresponding()[corresponding_id]);
+    socket
+      .getIo()
+      .to(socket.getCorresponding()[corresponding_id])
+      .emit("newMessage");
+  }
+
+  return res.status(200).end();
+};
 
 const test = async (req, res, next) => {
-    const io = socket.getIo();
+  const io = socket.getIo();
 
-    const resp = await io.allSockets();
+  const resp = await io.allSockets();
 
-    return res.status(200).json({
-        clients: [...resp],
-        corresponding: socket.getCorresponding()
-    })
-}
+  return res.status(200).json({
+    clients: [...resp],
+    corresponding: socket.getCorresponding(),
+  });
+};
 
 module.exports = {
-    getUsers,
-    getMessages,
-    sendMessage,
-    test,
+  getUsers,
+  getMessages,
+  sendMessage,
+  test,
 };
--- server/models/message.js
+++ server/models/message.js
@@ -1,22 +1,26 @@
 const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
-    sender: {
-      type: mongoose.Types.ObjectId,
-      required: [true, "Please add a sender"],
-    },
-    receiver: {
-      type: mongoose.Types.ObjectId,
-      required: [true, "Please add a receiver"],
-    },
-    message: {
-      type: String,
-      required: [true, "Please add a message"],
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now,
-    },
+  sender: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "Please add a sender"],
+  },
+  receiver: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "Please add a receiver"],
+  },
+  senderMessage: {
+    type: String,
+    required: [true, "Please add a message"],
+  },
+  receiverMessage: {
+    type: String,
+    required: [true, "Please add a message"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
